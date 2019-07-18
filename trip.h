6880ed1ceda945459e86987595fb05357d57638e
Jackson Steele <48249778+jackson-steele@users.noreply.github.com>
2019-07-17 19:47:04 -0600

    Add files via upload

---
+++ trip.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class trip
+{
+public:
+  trip();
+  ~trip();
+  PlanTrip(string origin, string destination, string startDate, string endDate, double proposedBudget, int numPeople);
+private:
+
+};
